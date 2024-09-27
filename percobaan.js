14dd13546710940eec659b4009d0e93956deaba6
nugnug7 <nugnug.develop777@gmail.com>
2024-09-27 21:10:32 +0700

    commit pertama saya

---
+++ percobaan.js
@@ -0,0 +1,13 @@
+//  memanggil modul bawaan dari node.js yaitu http 
+// untuk membuat server http
+
+const http = require('http')
+
+http.createServer(function(request,response) {
+    response.writeHead(200, {'content-type' : 'text/html'})
+    response.end('<h1>Selamat Datang di website saya Horaai...<\h1>')
+
+}).listen(3000, function() {
+    console.log('Server sudah nyala, http://localhost:3000')
+})
+
