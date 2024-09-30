30875feaacf6412cebd73311a7e693e4e89e402e
nugnug7 <nugnug.develop777@gmail.com>
2024-09-30 21:19:02 +0700

    Mengganti nama_depan dan nama_belakang

--- data_pribadi.js
+++ data_pribadi.js
@@ -3,9 +3,10 @@
 // let nama_lengkap = nama_depan + ' ' + nama_belakang
 
 
-let nama_depan = 'Levi'
-let nama_belakang = 'Ackerman'
+let nama_depan = 'Dwi'
+let nama_belakang = 'Nugroho'
 
+// Cara penulisan module exports (koneksi antar file/folder) yang sering di pakai
 module.exports = {
     nama_lengkap : nama_depan + ' ' + nama_belakang,
     alamat : 'Jakarta',
