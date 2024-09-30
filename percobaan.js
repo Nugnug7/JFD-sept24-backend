716d6a6ec30573621f10b8b730107ea7f7fda0fe
nugnug7 <nugnug.develop777@gmail.com>
2024-09-30 19:48:54 +0700

    Menambah mendeteksi halaman web

--- percobaan.js
+++ percobaan.js
@@ -6,11 +6,47 @@
 // git commit -m "ketik commit"
 // git push
 
+// Reset code coding balik ke masa sebelumnya dengan command sebagai berikut
+//git --hard (kode commit nya)
+
+
 const http = require('http')
 
 http.createServer(function(request,response) {
     response.writeHead(200, {'content-type' : 'text/html'})
-    response.end('<h1>Selamat Datang di website saya Horaai...<\h1>')
+    console.log(request.url)
+
+    // Halaman utama
+    if (request.url == '/') {
+        response.end(`<h1>Selamat datang di website</h1>`)
+    }
+    // halaman profil
+    else if (request.url == '/profil') {
+        response.end(
+            `<ul>
+                <li>Nama lengkap    : Dwi Nugroho</li>
+                <li> Nama panggilan : Dwi </li>
+                <li> Alamat         : Jakarta</li>
+                <li> Pekerjaan      : Freelance</li>
+            </ul>`
+        )
+    }
+    // halaman hubungi
+    else if (request.url == '/hubungi-saya') {
+        response.end(
+            `<ul>
+                <li>Wa      : 082122513110</li>
+                <li>Email   : dwi@gmail.com</li>
+            </ul>`
+        )
+        
+    }
+
+    // Untuk menangani URL yang tidak ada
+    else {
+        response.end(`<h1>Halaman tidak di temukan</h1>`)
+    }
+
 
 }).listen(3000, function() {
     console.log('Server sudah nyala, http://localhost:3000')
