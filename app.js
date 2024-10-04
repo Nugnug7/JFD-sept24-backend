8785fa29cdc8382f7704d3bf57ad5411297efa1c
nugnug7 <nugnug.develop777@gmail.com>
2024-10-04 20:12:48 +0700

    install express-validator & percobaan penggunaan

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 const mysql = require('mysql2')
-
-
+const {body, query, validationResult} = require('express-validator')
 
 // Menyambungkan ke sql 
 const db = mysql.createConnection ({
@@ -32,7 +31,6 @@ function getAll_karyawan () {
         LEFT JOIN jabatan as jbt ON jbt.id = kry.jabatan 
         LEFT JOIN agama as agm ON agm.id = kry.agama`
         
-
         db.query(sqlSyntax, function(errorSql, hasil) {
             if (errorSql) {
                 rejects(errorSql)
@@ -44,6 +42,34 @@ function getAll_karyawan () {
     })
 }
 
+function getOne_karyawan(idkry){
+    return new Promise ( (resolve, rejects) => {
+        let sqlSyntax =
+        `SELECT 
+            kry.*, jbt.nama as jabatan_nama, 
+            jbt.singkatan as jabatan_singkatan, 
+            agm.nama as agama_nama 
+        FROM karyawan as kry 
+        LEFT JOIN jabatan as jbt ON jbt.id = kry.jabatan 
+        LEFT JOIN agama as agm ON agm.id = kry.agama
+        where kry.id = ?`
+        
+        db.query(sqlSyntax, [idkry], function(errorSql, hasil) {
+            if (errorSql) {
+                rejects(errorSql)
+            } else {
+                resolve(hasil)
+            }
+    })
+
+    })
+
+}
+
+
+// Untuk mengambil data yang ter-encoded (enkripsi)
+// Yang dikirimkan melalui protokol http
+app.use(express.urlencoded({extended:false}))
 app.set('view engine', 'ejs')       // setting penggunaan template engine express
 app.set('views', './view-ejs')      // setting penggunaan untuk menyimpan file ejs
 
@@ -75,6 +101,92 @@ app.get('/karyawan', async function(req,res) {
     }
     res.render ('page-karyawan', data)
 })
+
+app.get('/karyawan/detail/:id_karyawan', async function(req,res) {
+    // Ambil 1 data karyawan
+    let data = {
+        satukaryawan : await getOne_karyawan(req.params.id_karyawan)
+    }
+    res.render ('page-karyawan-detail', data)
+})
+
+app.get('/karyawan/tambah', async function(req,res) {
+    // form tambah karyawan
+    res.render ('page-karyawan-form-tambah')
+})
+
+let formValidasiInsert = [
+    body('form_nik').notEmpty().isNumeric(),
+    body('form_nama').notEmpty().isString(),
+]
+
+app.post('/karyawan/proses-insert-data', formValidasiInsert,async function(req,res) {
+    const errors = validationResult(req)
+    // jika lolos validasi
+    if (errors.isEmpty()) {
+     // in case request params meet the validation criteria
+
+    // form tambah karyawan
+    // req.body             => ambil semua inputan dari form body
+    // req.body.nama_form   => ambil satuan inputan dari form body
+
+    try {
+         // 2. Kirim sebagai script SQL
+        let insert = await insert_karyawan(req)
+          // 3. Proses pengecekan ter input ke DB 
+        if (insert.affectedRows > 0)
+            res.redirect('/karyawan')
+            //console.log('Berhasil input ke Database')
+    // 3a. Jika gagal, tampilan error
+        } catch (error){
+        // 3b. Jika berhasil, tampilan pesan sukses
+        throw error
+        }
+    //insert_karywan (req)
+    //res.end('kiriman data')
+    // 1. Tangkap isi data dari masing-masing
+    
+    
+    // jika tidak lolos
+    }else {
+    // res.status(422).json({errors: errors.array()})
+    let errorData = {
+        pesanError: errors.array()
+    }
+    errorData.pesanError[0].fields
+    res.render('page-karyawan-form-tambah', errorData)
+}
+
+    
+})
+
+function insert_karyawan (req) {
+    return new Promise ( (resolve, rejects) => {
+        let sqlSyntax =
+        `insert INTO karyawan
+        (nama,nik,tanggal_lahir,alamat,jabatan,agama)
+        Values
+        (?,?,?,?,null,null)
+        `
+        let sqlData = [
+            req.body.form_nama,
+            req.body.form_nik,
+            req.body.form_tanggal_lahir,
+            req.body.form_alamat,
+        ]
+        
+        db.query(sqlSyntax, sqlData, function(errorSql, hasil) {
+            if (errorSql) {
+                rejects(errorSql)
+            } else {
+                resolve(hasil)
+            }
+    })
+
+    })
+
+}
+
 
 
 app.listen(3000, function() {
